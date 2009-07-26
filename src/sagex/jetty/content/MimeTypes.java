d5143880fc6ded2896b36b08cccf8bf046fd55a6
jreichen <jreichen>
2009-07-26 16:50:37 +0000

    - Add a location for a default mime.types delivered with the JettyStarter plugin. - Load multiple mime.types files, allowing user to take most defaults and only override selected file types.

--- src/sagex/jetty/content/MimeTypes.java
+++ src/sagex/jetty/content/MimeTypes.java
@@ -3,7 +3,9 @@ package sagex.jetty.content;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.mortbay.io.Buffer;
@@ -12,75 +14,85 @@ public class MimeTypes extends org.mortbay.jetty.MimeTypes
 {
     private static String MIME_FILE_LINE_DELIMITERS = "[\\t ]+"; // tab or space
 
-    private boolean isSetup;
     private long fileLastReadTime;
     private String location;
+    private List<File> mimeTypesFiles;
 
     public MimeTypes()
     {
-        setupMimeTypes();
     }
     
     private void setupMimeTypes()
     {
-        if (isSetup)
+        if (mimeTypesFiles == null)
         {
+            // mime types have not been setup or setLocation was called
+            findFiles();
+        }
+
+        if (mimeTypesFiles.size() == 0)
+        {
+            // no files were found
             return;
         }
 
-        // check if called within 30 secs (to prevent to much FS access)
-        if (fileLastReadTime + 30000 > System.currentTimeMillis())
+        // check if called within 1 second (to prevent too much FS access)
+        if (fileLastReadTime + 1000 > System.currentTimeMillis())
         {
             return;
         }
 
         try
         {
-            String fileLocation = getLocation();
-            
-            if (fileLocation == null)
+            // if any file has changed they must all be reloaded because
+            // one overrides another and types may be removed
+            boolean filesChanged = false;
+            for (File file : mimeTypesFiles)
             {
-                return;
-            }
-
-            File mimeTypesFile = new File(fileLocation);
-
-            // check if file has recently been loaded
-            if (fileLastReadTime >= mimeTypesFile.lastModified())
-            {
-                fileLastReadTime = System.currentTimeMillis();
-                return;
-            }
-
-            System.out.println("Loading mime types from file " + mimeTypesFile.getAbsolutePath());
-
-            Map<String, String> mimeMap = new HashMap<String, String>();
-            
-            BufferedReader reader = new BufferedReader(new FileReader(mimeTypesFile));
-            String line = null;
-            
-            while ((line = reader.readLine()) != null)
-            {
-                if (line.startsWith("#"))
+                if (fileLastReadTime <= file.lastModified())
                 {
-                    // comment
-                    continue;
+                    filesChanged = true;
+                    break;
                 }
-                String[] mimeTypeLineParts = line.split(MIME_FILE_LINE_DELIMITERS);
-                String mimeType = null;
-                if (mimeTypeLineParts.length > 1)
+            }
+
+            if (filesChanged)
+            {
+                Map<String, String> mimeMap = new HashMap<String, String>();
+
+                for (int i = mimeTypesFiles.size() - 1; i >= 0; i--)
                 {
-                    mimeType = mimeTypeLineParts[0];
-                    for (int i = 1; i < mimeTypeLineParts.length; i++)
+                    File mimeTypesFile = mimeTypesFiles.get(i);
+
+                    System.out.println("Loading mime types from file " + mimeTypesFile.getAbsolutePath());
+
+                    BufferedReader reader = new BufferedReader(new FileReader(mimeTypesFile));
+                    String line = null;
+
+                    while ((line = reader.readLine()) != null)
                     {
-                        mimeMap.put(mimeTypeLineParts[i], mimeType);
+                        if (line.startsWith("#"))
+                        {
+                            // comment
+                            continue;
+                        }
+                        String[] mimeTypeLineParts = line.split(MIME_FILE_LINE_DELIMITERS);
+                        String mimeType = null;
+                        if (mimeTypeLineParts.length > 1)
+                        {
+                            mimeType = mimeTypeLineParts[0];
+                            for (int j = 1; j < mimeTypeLineParts.length; j++)
+                            {
+                                mimeMap.put(mimeTypeLineParts[j], mimeType);
+                            }   
+                        }
                     }
                 }
+
+                this.setMimeMap(mimeMap);
             }
 
             fileLastReadTime = System.currentTimeMillis();
-
-            this.setMimeMap(mimeMap);
         }
         catch (Throwable t)
         {
@@ -91,6 +103,7 @@ public class MimeTypes extends org.mortbay.jetty.MimeTypes
     @Override
     public Buffer getMimeByExtension(String arg0)
     {
+        // lazy loading of mime types file(s)
         setupMimeTypes();
         return super.getMimeByExtension(arg0);
     }
@@ -98,60 +111,95 @@ public class MimeTypes extends org.mortbay.jetty.MimeTypes
     @Override
     public synchronized Map getMimeMap()
     {
+        // lazy loading of mime types file(s)
         setupMimeTypes();
         return super.getMimeMap();
     }
 
-    public String getLocation()
+    /**
+     * Overridden to synchronize the method
+     */
+    @Override
+    public synchronized void setMimeMap(Map mimeMap)
     {
+        super.setMimeMap(mimeMap);
+    }
+
+    private void findFiles()
+    {
+        mimeTypesFiles = new ArrayList<File>();
+
+        // use the sage home for relative paths
         File sageHome = new File(System.getProperty("user.dir"));
 
-        if (this.location != null)
+        File overrideMimeTypesFile = null;
+
+        if (getLocation() != null)
         {
             // the context xml file or calling code specified a location
-            File fileLocation = new File(location);
-            if (!fileLocation.isAbsolute())
+            overrideMimeTypesFile = new File(location);
+            if (!overrideMimeTypesFile.isAbsolute())
             {
                 // the location is not absolute so make it relative to the sagetv home
-                fileLocation = new File(sageHome, location);
+                overrideMimeTypesFile = new File(sageHome, location);
                 
-                if (fileLocation.exists())
+                if (overrideMimeTypesFile.exists())
                 {
-                    return fileLocation.getAbsolutePath();
+                    mimeTypesFiles.add(overrideMimeTypesFile);
                 }
                 else
                 {
                     System.out.println("mime.types file not found in override location " +
-                             fileLocation.getAbsolutePath() + ".  Checking default location.");
+                            overrideMimeTypesFile.getAbsolutePath() + ".  Checking default locations.");
                 }
             }
         }
 
-        // the default location
-        File mimeTypesFile = new File(sageHome, "jetty/user/mime.types");
+        // the user override location
+        File userMimeTypesFile = new File(sageHome, "jetty/user/mime.types");
+        File defaultMimeTypesFile = new File(sageHome, "jetty/etc/mime.types");
 
-        if (!mimeTypesFile.exists())
+        if (userMimeTypesFile.exists())
         {
-            // the backward-compatible location for nielm's webserver
-            File defaultMimeTypesFile = mimeTypesFile;
-            mimeTypesFile = new File(sageHome, "webserver/mime.types");
+            mimeTypesFiles.add(userMimeTypesFile);
+        }
 
-            if (!mimeTypesFile.exists())
+        if (defaultMimeTypesFile.exists())
+        {
+            mimeTypesFiles.add(defaultMimeTypesFile);
+        }
+
+        if (mimeTypesFiles.size() == 0)
+        {
+            if (getLocation() == null)
             {
-                System.out.println("mime.types file not found in default location " +
-                        defaultMimeTypesFile.getAbsolutePath() + " or legacy location " +
-                        mimeTypesFile.getAbsolutePath() + ".  Jetty's default mime types will " +
-                        "be the only types available.");
-                return null;
+                System.out.println("mime.types file for Jetty plugin not found in " +
+                    "user location " + userMimeTypesFile.getAbsolutePath() + " or " +
+                    "default plugin location " + defaultMimeTypesFile.getAbsolutePath() + ". " +
+                    "Jetty's default mime types will be the only types available.");
+            }
+            else
+            {
+                System.out.println("mime.types file for Jetty plugin not found in " +
+                    "user location " + userMimeTypesFile.getAbsolutePath() + ", " +
+                    "default plugin location " + defaultMimeTypesFile.getAbsolutePath() + ", or " +
+                    "web application override location + " + overrideMimeTypesFile.getAbsolutePath() + ", " +
+                    "Jetty's default mime types will be the only types available.");
             }
         }
-        
-        return mimeTypesFile.getAbsolutePath();
     }
-    
+
+    public String getLocation()
+    {
+        return this.location;
+    }
+
     public void setLocation(String location)
     {
         this.location = location;
+        // reset the file list so findFiles searches for files again
+        mimeTypesFiles = null;
+        findFiles();
     }
     
     public static void main(String[] args)
