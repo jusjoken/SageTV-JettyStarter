718fd172d0d14b80645d68cc6a615de2982a7456
jreichen <jreichen>
2010-05-15 16:48:26 +0000

    Create default values

--- src/sagex/jetty/properties/SagePropertiesImpl.java
+++ src/sagex/jetty/properties/SagePropertiesImpl.java
@@ -1,5 +1,6 @@
 package sagex.jetty.properties;
 
+import java.io.File;
 import java.util.Properties;
 
 import sagex.api.Configuration;
@@ -10,6 +11,31 @@ import sagex.api.Configuration;
 public class SagePropertiesImpl extends JettyProperties
 {
     private final static String JETTY_PROPERTY_PREFIX = "jetty";
+    
+    public SagePropertiesImpl()
+    {
+        // set up defaults if they don't exist (same defaults as the deprecated JettyStarter.properties file
+        String jettyHome = getProperty(JettyProperties.JETTY_HOME_PROPERTY);
+        if (jettyHome == null)
+        {
+            jettyHome = new File(System.getProperty("user.dir"), "jetty").getAbsolutePath();
+            setProperty(JettyProperties.JETTY_HOME_PROPERTY, jettyHome);
+        }
+
+        String configFiles = getProperty(JettyProperties.JETTY_CONFIG_FILES_PROPERTY);
+        if (configFiles == null)
+        {
+            configFiles = new File(jettyHome, "etc/jetty.xml").getAbsolutePath();
+            setProperty(JettyProperties.JETTY_CONFIG_FILES_PROPERTY, "\"" + configFiles + "\"");
+        }
+        
+        String logs = getProperty(JettyProperties.JETTY_LOGS_PROPERTY);
+        if (logs == null)
+        {
+            logs = new File(jettyHome, "logs").getAbsolutePath();
+            setProperty(JettyProperties.JETTY_LOGS_PROPERTY, logs);
+        }
+    }
 
     public Properties getProperties()
     {
@@ -18,7 +44,7 @@ public class SagePropertiesImpl extends JettyProperties
         String[] jettyPropertyArray = Configuration.GetSubpropertiesThatAreLeaves(JETTY_PROPERTY_PREFIX);
         for (String propertyName : jettyPropertyArray)
         {
-            String propertyValue = Configuration.GetProperty(JETTY_PROPERTY_PREFIX + "/" + propertyName, null);
+            String propertyValue = getProperty(propertyName);
             if (propertyValue != null)
             {
                 jettyProperties.put(propertyName, propertyValue);
@@ -30,5 +56,10 @@ public class SagePropertiesImpl extends JettyProperties
     public String getProperty(String propertyName)
     {
         return Configuration.GetProperty(JETTY_PROPERTY_PREFIX + "/" + propertyName, null);
+    }
+
+    private void setProperty(String propertyName, String value)
+    {
+        Configuration.SetProperty(JETTY_PROPERTY_PREFIX + "/" + propertyName, value);
     }
 }
