c6445a0fba40e60ee76e135b3d6f2f59b4e8e581
jreichen <jreichen>
2010-05-25 03:27:41 +0000

    *** empty log message ***

--- src/sagex/jetty/starter/Version.java
+++ src/sagex/jetty/starter/Version.java
@@ -11,12 +11,15 @@ public enum Version
     VERSION_1_4        ("1.4"),
     VERSION_1_5        ("1.5"),
     VERSION_1_6        ("1.6"),
-    VERSION_1_7        ("1.7"),
-    VERSION_1_7_1      ("1.7.1"),
-    VERSION_1_7_2      ("1.7.2"),
-    VERSION_1_7_3      ("1.7.3"),
-
-    CURRENT_VERSION    (VERSION_1_7_3.version);
+    VERSION_1_7        ("1.7"),   // Sage v7 beta
+    VERSION_1_7_1      ("1.7.1"), // Sage v7 beta
+    VERSION_1_7_2      ("1.7.2"), // Sage v7 beta
+    VERSION_1_7_3      ("1.7.3"), // Sage v7 beta
+    VERSION_1_7_4      ("1.7.4"), // Sage v7 beta
+    VERSION_1_7_5      ("1.7.5"), // Sage v7 beta
+    VERSION_2_0_0      ("2.0.0"), // Sage v7 beta
+    
+    CURRENT_VERSION    (VERSION_2_0_0.version);
     
     private String version = null;
 
